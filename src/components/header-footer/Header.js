0c490c46d8c8b2c0097f3ab8c2036b35a7c8243b
Tanmoy <104419571+TanmoyTheStudent@users.noreply.github.com>
2024-04-21 07:53:01 +0530

    Update Header.js

--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -24,26 +24,3 @@ function Header() {
 
 export default Header
 
-//make it responsive
-/* <div>
-<nav className="navbar navbar-expand-lg bg-warning">
-  <div className="container">
-    <div className="col-md-10">
-      <Link className="navbar-brand mb-0 h3 p-5" to="/">Home</Link>
-    </div>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/registration">Register</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</div> */
